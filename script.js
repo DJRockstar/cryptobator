f4843ce58579b26c9ba89386f24af22f0e105a17
DJRockstar <digvijays18@gmail.com>
2019-04-23 07:22:52 -0700

    Connected the First API logic to pull the Rates

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 'use strict'
 
-const searchURLCoinRank = "https://api.coinranking.com/v1/public/coins";
-const searchURLCryptoNator = "https://api.cryptonator.com/api/full/BTC-USD";
+
+function getData(coinSymbol){
+    const URLCoinRank = `https://api.coinranking.com/v1/public/coins?symbols=${coinSymbol}&timePeriod=24h`;
+    const URLCryptonator = `https://api.cryptonator.com/api/full/${coinSymbol}-USD`;
+    $(".card-container").empty();
+    fetch(URLCryptonator)
+    .then(response => response.json())
+    .then(responseJson => {
+        console.log(responseJson)
+        responseJson.ticker.markets.forEach((obj) => {
+            console.log(obj.market);
+            $("#result-card").append(
+               `<div class="card-container">
+                  <ul class="card-ul">
+                    <li><span class="li-name">Exchange:</span> <a href="#"> ${obj.market}</a></li>
+                    <li><span class="li-name">Price:</span> ${obj.price}</li>
+                  </ul>
+                </div>    
+                    `
+            )
+        })
+    })
+}
+
+
+function watchForm(){
+    $("form").on("submit", (e)=>{
+        e.preventDefault();
+        const coinSymbol = $("input:text").val();
+        getData(coinSymbol);
+    })
+}
+
+
+
+
+$(watchForm);
 
