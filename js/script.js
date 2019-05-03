e0970d2951e9cd3500d0504a38f42f07626b7fb5
DJRockstar <digvijays18@gmail.com>
2019-05-03 10:59:01 -0700

    Implemented logic to handle edge cases

--- js/script.js
+++ js/script.js
@@ -15,16 +15,19 @@ function fetchCryptonator(URLCryptonator){  //Req Res cycle for Cryptonator API
     .then(response => response.json())
     .then(responseJson => {
         displayResultsCNator(responseJson);
-    })
+    }).catch((err) => $("table").append(`<p> We apologize, No values found for this coin </p>`))
 }
 
 function fetchCoinRank(URLCoinRank){    //Req Res cycle for CoinRanking API
+    $(".wrong-ticker").empty();
     return fetch(URLCoinRank)
     .then(response => response.json())
     .then(responseJson => {
         displayResultsCRank(responseJson);
         displayChart(responseJson);
         displayCoinDescription(responseJson);
+    }).catch((err) => {
+        $("form").prepend("<p class='wrong-ticker'>Please enter a valid Ticker Symbol. Thank you :)</p>").css("text-aligh","center");
     })
 }
 
@@ -37,11 +40,11 @@ function displayResultsCRank(responseJson){     //Display results for CoinRankin
             <ul class="card-ul">
               <li><span class="li-name">Coin Name:</span> ${coinsObj.name}</li>
               <li><span class="li-name">Coin Rank:</span> ${coinsObj.rank}</li>
-              <li><span class="li-name">Volume(24h):</span> $${(coinsObj.volume/1000000000).toFixed(2)}M </li> 
+              <li><span class="li-name">Volume(24h):</span> $${(coinsObj.volume/1000000000).toFixed(3)}M </li> 
               <li><span class="li-name">Coin URL:</span> <a class="coin-url" href="${coinsObj.websiteUrl}">${coinsObj.websiteUrl}</a></li>
-              <li><span class="li-name">Circulating Supply:</span> $${(coinsObj.circulatingSupply/1000000).toFixed(2)}M</li>
+              <li><span class="li-name">Circulating Supply:</span> $${(coinsObj.circulatingSupply/1000000).toFixed(3)}M</li>
               <li><span class="li-name">Confirmed Supply:</span> ${coinsObj.confirmedSupply}</li>
-              <li><span class="li-name">Market Cap:</span> $${(coinsObj.marketCap/1000000000).toFixed(2)}M</li>
+              <li><span class="li-name">Market Cap:</span> $${(coinsObj.marketCap/1000000000).toFixed(3)}M</li>
             </ul>
         </div>    
             `)
@@ -62,7 +65,7 @@ function displayResultsCNator(responseJson){        //Display results for Crypto
         ` <tbody>
               <tr class="row-body">
                 <td class="ex-name">${obj.market}</td>
-                <td class="price">${parseInt((obj.price)).toFixed(2)}</td>
+                <td class="price">${parseFloat((obj.price)).toFixed(4)}</td>
               </tr>
           </tbody>
         `
@@ -74,10 +77,16 @@ function displayResultsCNator(responseJson){        //Display results for Crypto
 function displayCoinDescription(responseJson){ //Displays descriptoon about the Coin
     $("#coin-desc-id").removeClass("hidden");
     const coinsObj = responseJson.data.coins[0];
-    $("#coin-desc-id").append(`
-    <div class="coin-desc-cointainer">
-        <p class="coin-description">What is ${coinsObj.name}? <br><br> ${coinsObj.description}</p>
-    </div>`)
+    if(coinsObj.description===null){
+        $("#coin-desc-id").append(`<p> We apologize, No description found for this coin </p>`)
+    }
+    else {
+        $("#coin-desc-id").append(`
+        <div class="coin-desc-cointainer">
+            <p class="coin-description">What is ${coinsObj.name}? <br><br> ${coinsObj.description}</p>
+        </div>`)
+    }
+    
 }
 
 
